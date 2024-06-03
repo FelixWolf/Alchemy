98dc2665e01c18eb79869ae8281819c1468899c6
Rye Mutt <rye@alchemyviewer.org>
2024-06-03 16:48:55 -0400

    More fixes

--- indra/lscript/lscript_execute/lscript_execute.cpp
+++ indra/lscript/lscript_execute/lscript_execute.cpp
@@ -4033,11 +4033,10 @@ void lscript_run(const std::string& filename, BOOL b_debug)
         // to check how to abort or error out gracefully
         // from this function. XXXTBD
     }
-    LLFILE* file = LLFile::fopen(filename, "r");  /* Flawfinder: ignore */
+    LLFILE* file = LLFile::fopen(filename, "rb");  /* Flawfinder: ignore */
     if(file)
     {
         execute = new LLScriptExecuteLSL2(file);
-        fclose(file);
     }
     if (execute)
     {
@@ -4060,8 +4059,8 @@ void lscript_run(const std::string& filename, BOOL b_debug)
         printf("bp: 0x%X\n", get_register(execute->mBuffer, LREG_BP));
         printf("hr: 0x%X\n", get_register(execute->mBuffer, LREG_HR));
         printf("hp: 0x%X\n", get_register(execute->mBuffer, LREG_HP));
+        printf("faults %d\n", execute->getFaults());
         delete execute;
-        fclose(file);
     }
 }
 
